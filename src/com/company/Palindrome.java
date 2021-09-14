5f46dffec9fac5ec84d7ffd99f14011912898c80
taticd <acespedesd@gmail.com>
2021-09-13 18:18:56 -0600

    create palindrome class

---
+++ src/com/company/Palindrome.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.text.Normalizer;
+import java.util.Scanner;
+
+public class Palindrome {
+
+    public void findPalindrome(){
+        Scanner scanner = new Scanner(System.in);
+        String word;
+        System.out.println("Enter a word");
+        word = scanner.nextLine();
+        String wordResult = word.replace(" ", "");
+        String wordReplacement =wordResult.replace(",","");
+        String stringNormalize = Normalizer.normalize(wordReplacement, Normalizer.Form.NFD);
+        String stringWithoutAccents = stringNormalize.replaceAll("[^\\p{ASCII}]", "");
+        String invertedWord = "";
+        for(int contador = stringWithoutAccents.length() - 1; contador>=0; contador--){
+
+
+
+            invertedWord+= stringWithoutAccents.charAt(contador);
+
+        }
+
+        if(stringWithoutAccents.equalsIgnoreCase(invertedWord)){
+            System.out.println("Its palindrome");
+
+        }else{
+            System.out.println("Not is palindrome");
+        }
+
+
+    }
+    }
+}
